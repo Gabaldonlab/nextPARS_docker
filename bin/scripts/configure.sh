9739c5c153a81c5763e045598a43aad2d533f1b8
ucielp <ucielp@gmail.com>
2020-01-31 17:16:54 +0100

    configure modified

--- bin/scripts/configure.sh
+++ bin/scripts/configure.sh
@@ -4,9 +4,11 @@ DATAPATH=/home/nextPARS/data
 BINPATH=/home/nextPARS/bin
 ln -sfn /usr/bin/python3.5 /usr/bin/python
 
-mkdir $DATAPATH/fastq
-mkdir $DATAPATH/fastq
-mkdir $DATAPATH/mapping
-mkdir $DATAPATH/score
-mkdir $DATAPATH/tab
-mkdir $DATAPATH/trimming
+mkdir -p $DATAPATH/fastq
+mkdir -p $DATAPATH/fastq
+mkdir -p $DATAPATH/mapping
+mkdir -p $DATAPATH/score
+mkdir -p $DATAPATH/tab
+mkdir -p $DATAPATH/trimming
+
+chmod -R 775 $DATAPATH
