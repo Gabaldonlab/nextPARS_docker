2c8c5ac025557fe79cba0f5f86114fd98842ff1e
ucielp <ucielp@gmail.com>
2020-01-31 17:08:08 +0100

    Added direcory structure

--- bin/scripts/configure.sh
+++ bin/scripts/configure.sh
@@ -4,3 +4,9 @@ DATAPATH=/home/nextPARS/data
 BINPATH=/home/nextPARS/bin
 ln -sfn /usr/bin/python3.5 /usr/bin/python
 
+mkdir $DATAPATH/fastq
+mkdir $DATAPATH/fastq
+mkdir $DATAPATH/mapping
+mkdir $DATAPATH/score
+mkdir $DATAPATH/tab
+mkdir $DATAPATH/trimming
